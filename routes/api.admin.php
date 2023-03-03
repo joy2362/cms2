01f8adb96fcd0c865722aa4e2238d35c30f9e29e
joy2362 <abdullahzahidjoy@gmail.com>
2023-03-03 11:03:37 +0600

    refactor admin route

--- routes/api.admin.php
+++ routes/api.admin.php
@@ -4,23 +4,31 @@ use App\Http\Controllers\Backend\{Auth\AdminAuthController, Auth\LoginController
 use Illuminate\Support\Facades\Route;
 
 
+
 /*
- * Admin authentication related route
- *
- * */
+|--------------------------------------------------------------------------
+| Admin authentication related route
+|--------------------------------------------------------------------------
+*/
 Route::post('/login', LoginController::class);
 
 
 Route::middleware('auth:sanctum')
     ->group(function () {
-
-        //auth related route
+        /*
+        |--------------------------------------------------------------------------
+        | Auth related route
+        |--------------------------------------------------------------------------
+        */
         Route::controller(AdminAuthController::class)->group(function () {
             Route::get('/me', 'me');
             Route::post('/logout', 'logout');
         });
-
-        //profile related route
+        /*
+        |--------------------------------------------------------------------------
+        | profile related route
+        |--------------------------------------------------------------------------
+        */
         Route::controller(ProfileController::class)->prefix('profile')->group(function () {
             Route::get('/', 'profile');
             Route::post('/password', 'changePassword');
