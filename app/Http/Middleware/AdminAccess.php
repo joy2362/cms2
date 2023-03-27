ca4291687faf0691d867d0e005d8932cbfc85e03
joy2362 <joy2362@users.noreply.github.com>
2023-03-27 15:59:18 +0000

    php linting

--- app/Http/Middleware/AdminAccess.php
+++ app/Http/Middleware/AdminAccess.php
@@ -6,7 +6,6 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 class AdminAccess
 {
