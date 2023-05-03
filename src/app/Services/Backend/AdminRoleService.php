639bb5a507cdf9762a3d5011d933115425548e29
joy2362 <joy2362@users.noreply.github.com>
2023-05-03 16:52:01 +0000

    php linting

--- src/app/Services/Backend/AdminRoleService.php
+++ src/app/Services/Backend/AdminRoleService.php
@@ -72,7 +72,8 @@ class AdminRoleService
 
     public function getPermissions(): Collection
     {
-        $permissions = Permission::where('guard_name', 'admin')->get()->groupBy('group_name');;
+        $permissions = Permission::where('guard_name', 'admin')->get()->groupBy('group_name');
+        ;
         return $this->success(['permissions' => $permissions]);
     }
 }
